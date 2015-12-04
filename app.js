1249387a17616d76dd2c4b6ceaed11e9dbf38ad2
JLModra <754675419@qq.com>
2015-12-04 11:15:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path    = require("path");
 
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/webdemo/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
